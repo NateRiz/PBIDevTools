6cdc57001d2d628855d1aa4832a4b962cb5dc25f
NateRiz <nathan.rizik@gmail.com>
2022-02-02 11:04:30 -0800

    perf

--- PbiClients.js
+++ PbiClients.js
@@ -14,6 +14,7 @@ if (window.PbiDevPbiClientsInjected === undefined){
     var connectionStrings = []
     var dataSourceIndex = 0
     var isRenderComplete = false
+    var isPerfTesting = false
 }
 
 function isPingUrl(url){
@@ -142,6 +143,15 @@ function createModal(){
             dataSourceIndex = ((dataSourceIndex - 1) === -1 ? dataProviders.length-1 : dataSourceIndex - 1)
             setDataSource(dataSourceIndex)
         }
+
+        document.querySelector("#PbiDevStartPerf").onclick = () => {
+            var testAmount = document.querySelector("#PbiDevTestAmount").value
+            clearSessionStorage()
+            createSessionStorage()
+            reloadPageForPerf(1, Number(testAmount) || 999)
+        }
+
+        disablePageIfPerfTesting()
     });
 }
 
@@ -226,6 +236,12 @@ function fetchRdl(){
 
 function downloadRdl(){
     fetchRdl().then((data)=>download("Download.rdl", data))
+}
+
+function setAllowSessionExpiration(isChecked){
+    document.querySelector("#PbiDevPingToggle").checked = isChecked
+    toggleKeepSessionAlive()
+
 }
 
 function toggleKeepSessionAlive(){
@@ -371,6 +387,142 @@ function updateSessionTimer(timeSinceLastInteractionMs){
     updateToolbarResult("PbiDevTTLBG", time)
 }
 
+function clearSessionStorage(){
+    window.sessionStorage.removeItem('PbiDevPerf')
+}
+
+function createSessionStorage(){
+    window.sessionStorage.setItem('PbiDevPerf', JSON.stringify({
+        "timeRenderingms": [],
+        "timeProcessingms": []
+    }))
+}
+
+function addPerfMeasurementToSessionStorage(rendering, processing){
+    data = window.sessionStorage.getItem('PbiDevPerf')
+    json = JSON.parse(data)
+
+    if (json !== null){
+        json["timeRenderingms"].push(rendering)
+        json["timeProcessingms"].push(processing)
+    }
+    window.sessionStorage.setItem('PbiDevPerf', JSON.stringify(json))
+}
+
+function reloadPageForPerf(currentTestNumber, totalTestAmount) {
+    url = new URL(document.URL)
+    url.searchParams.set("PbiDevCurrent", currentTestNumber)
+    url.searchParams.set("PbiDevEnd", totalTestAmount)
+    var nextPage = url.href
+    window.location = nextPage
+}
+
+function disablePageIfPerfTesting(){
+    url = new URL(document.URL)
+    var curRender = Number(url.searchParams.get("PbiDevCurrent") || -1)
+    var endRender = Number(url.searchParams.get("PbiDevEnd") || -1)
+    isPerfTesting = curRender !== -1 && endRender !== -1
+    if (!isPerfTesting){
+        return;
+    }
+    
+    setAllowSessionExpiration(false)
+    document.querySelector("#PbiDevScreenBlock").hidden = false
+    document.querySelector("#PbiDevTestStatus").textContent = `${curRender} of ${endRender}`
+}
+
+function endPerformanceTests(){
+    createAndDownloadPerfReport(()=>{
+        url = new URL(document.URL)
+        url.searchParams.delete("PbiDevCurrent")
+        url.searchParams.delete("PbiDevEnd")
+        var nextPage = url.href
+        window.location = nextPage
+    })
+}
+
+function percentile(list, p) {
+    p = Number(p);
+    list = list.slice().sort(function (a, b) {
+      a = Number.isNaN(a) ? Number.NEGATIVE_INFINITY : a;
+      b = Number.isNaN(b) ? Number.NEGATIVE_INFINITY : b;
+      if (a > b) return 1;
+      if (a < b) return -1;
+      return 0;
+    });
+  
+    if (p === 0) return list[0];
+    var kIndex = Math.ceil(list.length * (p / 100)) - 1;
+    return list[kIndex];
+  }
+
+function createAndDownloadPerfReport(onCompletion){
+    data = window.sessionStorage.getItem('PbiDevPerf')
+    json = JSON.parse(data)
+    renderingData = []
+    processingData = []
+
+    if (json !== null){
+        renderingData = json["timeRenderingms"]
+        processingData = json["timeProcessingms"]
+    }
+
+    fetch(chrome.extension.getURL('/Performance.rdl'))
+    .then((resp) => resp.text())
+    .then((resp) => {
+        build=""
+        renderingData.forEach(function (val, idx) {
+            build+=`<Data ColumnIndex="0" RowIndex="${idx}">${val}</Data>\n`
+        })
+        resp = resp.replace('{PbiDevRenderMatrix}', build)
+
+        build=""
+        renderingData.forEach(function (val, idx) {
+            build+=`&lt;Row&gt; &lt;RenderEnterData&gt;${val}&lt;/RenderEnterData&gt; &lt;/Row&gt;\n`
+        })
+        resp = resp.replace('{PbiDevRenderData}', build)
+
+        build=""
+        processingData.forEach(function (val, idx) {
+            build+=`<Data ColumnIndex="1" RowIndex="${idx}">${val}</Data>\n`
+        })
+        resp = resp.replace('{PbiDevProcessMatrix}', build)
+
+        build=""
+        processingData.forEach(function (val, idx) {
+            build+=`&lt;Row&gt; &lt;ProcessingEnterData&gt;${val}&lt;/ProcessingEnterData&gt; &lt;/Row&gt;\n`
+        })
+        resp = resp.replace('{PbiDevProcessData}', build)
+        
+        resp = resp.replace("{rendermin}", Math.min(...renderingData))
+        resp = resp.replace("{rendermax}", Math.max(...renderingData))
+        resp = resp.replace("{renderrange}", Math.max(...renderingData)-Math.min(...renderingData))
+        resp = resp.replace("{renderavg}", renderingData.reduce((a, b) => a + b) / renderingData.length)
+        resp = resp.replace("{render50}", percentile(renderingData, 50))
+        resp = resp.replace("{render75}", percentile(renderingData, 75))
+        resp = resp.replace("{render96}", percentile(renderingData, 96))
+        resp = resp.replace("{render99}", percentile(renderingData, 99))
+
+        resp = resp.replace("{processingmin}", Math.min(...processingData))
+        resp = resp.replace("{processingmax}", Math.max(...processingData))
+        resp = resp.replace("{processingrange}", Math.max(...processingData)-Math.min(...processingData))
+        resp = resp.replace("{processingavg}", processingData.reduce((a, b) => a + b) / processingData.length)
+        resp = resp.replace("{processing50}", percentile(processingData, 50))
+        resp = resp.replace("{processing75}", percentile(processingData, 75))
+        resp = resp.replace("{processing96}", percentile(processingData, 96))
+        resp = resp.replace("{processing99}", percentile(processingData, 99))
+
+        download("Performance.rdl", resp)
+        
+        csv = `RenderingTime_ms,${renderingData.join(",")}\nProcessingTime_ms,${processingData.join(",")}`
+
+        download("Performance.csv", csv)
+
+        console.log(resp)
+        onCompletion()
+    })
+}
+
 function pollRenderStatus(){
     if (isRenderComplete || rdlWorkloadUrl === "" || renderId === "" || bearerToken === ""){
         return;
@@ -397,6 +549,19 @@ function onRenderComplete(response){
     timeProcessingLabel.textContent = response.timeProcessingms
     timeRenderingLabel.textContent = response.timeRenderingms
     contentSizeLabel.textContent = response.contentSize
+
+    if (isPerfTesting){
+        var nextRender = Number(url.searchParams.get("PbiDevCurrent"))+1
+        var endRender = Number(url.searchParams.get("PbiDevEnd"))
+        addPerfMeasurementToSessionStorage(Number(response.timeRenderingms), Number(response.timeProcessingms))
+
+        if (nextRender <= endRender){
+            reloadPageForPerf(nextRender, endRender)
+        }
+        else {
+            endPerformanceTests()
+        }
+    }
 }
 
 function networkDispatcher(message, sender, sendResponse){
